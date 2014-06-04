82250cd8ad522d2c0a996227fb7235c4660f8199
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-06-04 22:36:21 +0900

    add coffee script setting

--- pizaid/pizaid/settings.py
+++ pizaid/pizaid/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djcompass'
+    'djcompass',
+    'coffeescript'
 )
 
 MIDDLEWARE_CLASSES = (
