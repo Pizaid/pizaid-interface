9d3e9aac9e9751f248a2d11755072c0768d91766
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-06-07 13:07:44 +0900

    Add pizaidnetwork using dbus

--- pizaid/interface/contents/dbusnetwork.py
+++
@@ -1,6 +0,0 @@
-class DBusNetwork():
-    def __init__(self):
-        return
-
-    def getIPv4(self):
-        return "0.0.0.0"
--- pizaid/interface/views.py
+++ pizaid/interface/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render_to_response
-from contents.contents import get_network
+from dbusinterface.pizaidnetwork import PizaidNetwork
 
 # Create your views here.
 def index(request):
     return render_to_response('interface/index.html')
 
 def status(request):
-    network = get_network()    
+    network = PizaidNetwork()
+    properties = network.get_properties()
     return render_to_response('interface/status.html',
                               {
-                                  'ipv4': network.getIPv4()
+                                  'ipv4': properties.Get_ipv4()
                               })
 
 def info(request):
