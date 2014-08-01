2d478827825635431c86a13bfe658dad03c9531f
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-08-01 23:22:06 +0900

    add descption and remove unnecessary description

--- pizaid/pizaid/wsgi.py
+++ pizaid/pizaid/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizaid.settings")
+sys.path.append("~/GitHub/pizaid-interface/pizaid")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
