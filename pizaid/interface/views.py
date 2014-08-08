c036479bfa8b63ffc867e02201d03b94ea68826f
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-08-09 04:55:30 +0900

    Add magic comment and remove unnecessary files

--- pizaid/interface/views.py
+++ pizaid/interface/views.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+#
+# Author:   Makoto Shimazu <makoto.shimaz@gmail.com>
+# URL:      https://github.com/Pizaid
+# License:  2-Clause BSD License
+# Created:  2014-08-09
+#
+
 from django.shortcuts import render_to_response
 # from dbusinterface.pizaidnetwork import PizaidNetwork
 # from dbusinterface.pizaidstorage import PizaidStorage
