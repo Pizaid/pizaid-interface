f1421e6bec2040a6a47fe472ea458830fd8428d5
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-09-01 23:26:44 +0900

    Add .scss and .js for displaying battery remaining quantity

---
+++ pizaid/static/js/battery_bullet_chart.js
@@ -0,0 +1,49 @@
+$(function() {
+  // http://bl.ocks.org/mbostock/4061961
+  var
+    margin = { top: 10, right: 10, bottom: 20, left: 70 },
+    width = 400 - margin.left - margin.right,
+    height = 60 - margin.top -  margin.bottom,
+    data_arr = [
+      {
+        "title": "battery",
+        "subtitle": "%",
+        "ranges": [30, 60, 100],
+        "measures": [42],
+        "markers": [100],
+      }
+    ];
+
+  var chart = d3.bullet()
+                .width(width)
+                .height(height);
+
+  var svg = d3.select("#battery")
+                .selectAll("svg")
+                .data(data_arr)
+              .enter().append("svg")
+                .attr("class", "bullet")
+                .attr("font-size", "10px")
+                .attr("font-family", "sans-serif")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .call(chart);
+
+  var title = svg.append("g")
+        .style("text-anchor", "end")
+        .attr("transform", "translate(-6," + height / 2 + ")");
+
+  title.append("text")
+    .attr("class", "title")
+    .attr("font-size", "14px")
+    .attr("font-weight", "bold")
+    .text(function(d) { return d.title; });
+
+  title.append("text")
+    .attr("class", "subtitle")
+    .attr("fill", "#999")
+    .attr("dy", "1.5em")
+    .text(function(d) { return d.subtitle; });
+});
